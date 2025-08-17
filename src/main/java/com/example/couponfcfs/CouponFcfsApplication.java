3481f9ea92eb3135e272574f538b07ee3b49a8c7
kk787016 <wnsdnr1327@naver.com>
2025-08-17 17:35:55 +0900

    Initial commit

---
+++ src/main/java/com/example/couponfcfs/CouponFcfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.couponfcfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponFcfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponFcfsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/couponfcfs/CouponFcfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.couponfcfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponFcfsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
