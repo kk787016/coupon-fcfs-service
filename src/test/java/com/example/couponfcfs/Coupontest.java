922081660109cf43ff25f43020e2f590a1967410
kk787016 <wnsdnr1327@naver.com>
2025-08-20 15:09:46 +0900

    feat: test 시작

---
+++ src/test/java/com/example/couponfcfs/Coupontest.java
@@ -0,0 +1,88 @@
+package com.example.couponfcfs;
+
+import com.example.couponfcfs.dto.ResponseDto;
+import com.example.couponfcfs.service.CouponService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class Coupontest {
+
+    private final CouponService couponService;
+    private final RedisTemplate<String, Object> redisTemplateForCoupon;
+
+    @Autowired
+    public Coupontest(CouponService couponService, RedisTemplate<String, Object> redisTemplateForCoupon) {
+        this.redisTemplateForCoupon = redisTemplateForCoupon;
+        this.couponService  = couponService;
+    }
+
+    @BeforeEach
+    public void setUp() {
+
+
+        HashOperations<String, String, String> hash = redisTemplateForCoupon.opsForHash();
+
+        hash.put("A","1","0");
+
+        for(int i = 1; i <= 30; i++){
+            hash.put("B", String.valueOf(i),"0");
+        }
+
+        for(int i = 1; i <= 69; i++){
+            hash.put("C", String.valueOf(i),"0");
+        }
+        hash.put("Stock","A","1");
+        hash.put("Stock","B","30");
+        hash.put("Stock","C","69");
+
+        hash.put("StockCheck","A","0");
+        hash.put("StockCheck","B","0");
+
+    }
+
+    @Test
+    void couponTes1t() throws InterruptedException {
+
+        HashOperations<String, String, String> hash = redisTemplateForCoupon.opsForHash();
+
+        int threadCount = 200;
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try{
+                    System.out.println(
+                            "1"
+                    );
+                    //ResponseDto responseDto = couponService.selectCoupon("sd");
+                }finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Map<String, String> entries = hash.entries("StockCheck");
+
+        int num = 0;
+        for(Map.Entry<String, String> entry : entries.entrySet()) {
+            int value = Integer.parseInt(entry.getValue());
+            num = num + value;
+        }
+        Assertions.assertThat(num).isEqualTo(threadCount);
+    }
+}
